290518fd101d27c054207eebfb1760776d489ecc
zahrafw15 <97815989+zahrafw15@users.noreply.github.com>
2022-01-16 14:53:27 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import "./App.css";
+import "./Assets/App.scss";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="navbar">
+        header
+      </div>
+
+      <div className="section">
+        <div className="card section-body-1">1</div>
+        <div className="card section-body-2">2</div>
+        <div className="card section-body-3">3</div>
+        <div className="card section-body-4">4</div>
+        <div className="card section-body-5">5</div>
+        <div className="card section-body-6">6</div>
+      </div>
+      <div className= "body">
+        <div className="anak-body anak-1">1</div>
+
+        <div className="anak-body anak-2">2</div>
+           <div className="anak anak-1">1</div>
+           <div className="anak anak-2">2</div>
+
+        <div className="anak-body anak-3">3</div>
+  
+      </div>
+    </div>
+  );
+}
+
+export default App;
